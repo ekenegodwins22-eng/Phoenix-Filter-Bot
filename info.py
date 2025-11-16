9791f90b44545ff75bab4b9d8bec41808eb5c03b
phoenix1186 <ekenegodwins22@gmail.com>
2025-11-16 04:05:31 -0500

    Fix: Remove extraneous characters ({find:}) from OWNER_LNK line in info.py.

--- info.py
+++ info.py
@@ -91,7 +91,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/mfilerequest')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Phoenixmoviesfiles')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'mfilesupportgroup') # Support Chat Link Without https:// or @
-OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/ph0enix_web')},{find:
+OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/ph0enix_web')
 
 # True Or False
 AI_SPELL_CHECK = bool(environ.get('AI_SPELL_CHECK', True))
