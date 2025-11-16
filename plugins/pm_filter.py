4cb5a8b92aeef9f85e0dc268e5a76954161fa55d
phoenix1186 <ekenegodwins22@gmail.com>
2025-11-16 05:20:05 -0500

    Final Security Fix: Removed 'Source Code' button from /about command.

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1962,7 +1962,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
     elif query.data == "about":
         buttons = [[
             InlineKeyboardButton('Sᴜᴘᴘᴏʀᴛ Gʀᴏᴜᴘ', url=GRP_LNK),
-            InlineKeyboardButton('Sᴏᴜʀᴄᴇ Cᴏᴅᴇ', url="https://github.com/phoenix1186/PHOENIX-FILTER-BOT")
         ],[
             InlineKeyboardButton('Hᴏᴍᴇ', callback_data='start'),
             InlineKeyboardButton('Cʟᴏsᴇ', callback_data='close_data')
